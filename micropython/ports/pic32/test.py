637f7a75c4a6263335dc00d1609bc0518ef07564
Paul Bruggeman <paul@killercats.com>
2018-10-06 09:41:55 -0400

    more better

---
+++ micropython/ports/pic32/test.py
@@ -0,0 +1,13 @@
+print('word ', list(x+1 for x in range(4)), end=' eol\n')
+
+for i in range(4):
+	print(i)
+
+
+import pybadge
+p=pybadge
+p.FbColor(0b1111111111111111)
+p.FbWriteLine('testing')
+
+
+
