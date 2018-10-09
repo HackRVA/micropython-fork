aec40c4c1fde78fcef8cf522fc33456cb1455bb5
Paul Bruggeman <paul@killercats.com>
2018-10-09 14:01:39 -0400

    sendbadge.py can now read from stdin

--- micropython/ports/pic32/sendbadge.py
+++ micropython/ports/pic32/sendbadge.py
@@ -33,5 +33,28 @@ def readFile(filename):
 	ser.close()
 	return
 
+def readstdin():
+	port = "/dev/ttyACM0"
+	lines = []
 
-readFile('test.py')
+	#ser = serial.Serial(port, 57600, timeout=0)
+	ser = serial.Serial(port, 115200, timeout=0)
+
+	lineno = 0
+	for line in sys.stdin:
+	    lineno += 1
+
+	    ser.write(line)
+	    ser.flush()
+	    print 'sent', lineno, line
+
+	    for r in range(1):
+		data = ser.read(9999)
+		if len(data) > 0:
+			print '						recv:', data
+
+	return
+
+
+readstdin()
+#readFile('test.py')
